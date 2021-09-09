cffb3bfa7fcd4f9a98e6386d09844450b25ee7d1
Raul Smith <raul.n.smith@gmail.com>
2021-09-09 15:53:16 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Welcome to the IMDb Random Generator</h1>
+    <h2>Click on the button below and find a random movie on the Internet Movie Database</h2>
+    <RandomGenerator />
+  </div>
+</template>
+
+<script>
+import RandomGenerator from './components/RandomGenerator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RandomGenerator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
